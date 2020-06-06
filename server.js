c96053075c5a10991fdcce8bf26080fb95c75688
sunny <sunny_ye@outlook.com>
2020-06-07 02:10:23 +0800

    fix server script

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const db = require('./js/datastorage.js');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const httpServer = require('http').Server(app);
 const io = require('socket.io')(httpServer);
 
